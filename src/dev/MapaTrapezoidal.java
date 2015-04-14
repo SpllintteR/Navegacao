b839b7316ff86166e5f6e4e055eba5c0a978522e
Paulo Weber <pauloweber@hotmail.com.br>
2015-04-14 18:44:45 -0300

    PrototipoMapaTrapezoidal

--- src/dev/MapaTrapezoidal.java
+++ src/dev/MapaTrapezoidal.java
@@ -1,14 +1,126 @@
 package dev;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MapaTrapezoidal {
 
-	private int[][]	pontos;
+	private int[][]		pontos;
+	private Ponto		inicio;
+	private Ponto		fim;
+
+	private static int	VERDE		= 1;
+	private static int	AMARELO		= 2;
+	private static int	BRANCO		= 0;
+	private static int	INICIO		= 3;
+	private static int	OBSTACULO	= -1;
+	private static int	AZUL		= 4;
+
+	private static int	NORMAL		= 165;
+	private static int	MAX			= 330;
+
+	private static int	FRENTE		= 410;
 
 	public static void main(final String[] args) {
 		MapaTrapezoidal mapa = new MapaTrapezoidal();
+		// Cenario cenario = montarCenario1();
+		Cenario cenario = montarCenario2();
+		mapa.calcularPontosMediosEIntersecoes(cenario);
+
+		for (int i = 0; i < mapa.pontos.length; i++) {
+			String str = "";
+			for (int j = 0; j < mapa.pontos[i].length; j++) {
+				str += mapa.pontos[i][j] + " ";
+			}
+			System.out.println(str);
+		}
+
+		List<Passo> passos = new ArrayList<Passo>();
+		mapa.encontrarPassos(passos, mapa.inicio, null);
+		Passo last = Passo.CIMA;
+		boolean first = true;
+		while (passos.size() > 0) {
+			if (Button.ESCAPE.isDown()) {
+				break;
+			}
+			Passo p = passos.get(0);
+			System.out.println(p);
+			if (p != last) {
+				if (p == Passo.DIREITA) {
+					if (last == Passo.CIMA) {
+						giraParaDirieta();
+					} else {
+						if (last == Passo.BAIXO) {
+							giraParaEsquerda();
+						} else {
+							inverter();
+						}
+					}
+				} else {
+					if (p == Passo.ESQUERDA) {
+						if (last == Passo.CIMA) {
+							giraParaEsquerda();
+						} else {
+							if (last == Passo.BAIXO) {
+								giraParaDirieta();
+							} else {
+								inverter();
+							}
+						}
+					} else {
+						if (p == Passo.CIMA) {
+							if (last == Passo.DIREITA) {
+								giraParaEsquerda();
+							} else {
+								if (last == Passo.ESQUERDA) {
+									giraParaDirieta();
+								} else {
+									inverter();
+								}
+							}
+						} else {
+							if (last == Passo.DIREITA) {
+								giraParaDirieta();
+							} else {
+								if (last == Passo.ESQUERDA) {
+									giraParaEsquerda();
+								} else {
+									inverter();
+								}
+							}
+						}
+					}
+				}
+			}
+			if (first) {
+				Motor.A.rotate(-FRENTE - 200, true);
+				Motor.C.rotate(-FRENTE - 200);
+				first = false;
+			} else {
+				Motor.A.rotate(-FRENTE, true);
+				Motor.C.rotate(-FRENTE);
+			}
+			last = p;
+			passos.remove(0);
+		}
+		giraParaDirieta();
+	}
+
+	private static Cenario montarCenario2() {
+		Cenario cenario = new Cenario();
+		cenario.setLinhas(7);
+		cenario.setColunas(6);
+		cenario.setInicio(new Ponto(3, 0));
+		cenario.setFim(new Ponto(6, 5));
+		cenario.addPontoInvalido(new Ponto(1, 2));
+		cenario.addPontoInvalido(new Ponto(2, 3));
+		cenario.addPontoInvalido(new Ponto(3, 4));
+		cenario.addPontoInvalido(new Ponto(3, 1));
+		cenario.addPontoInvalido(new Ponto(4, 2));
+		cenario.addPontoInvalido(new Ponto(5, 4));
+		cenario.addPontoInvalido(new Ponto(5, 2));
+		cenario.addPontoInvalido(new Ponto(6, 3));
+		return cenario;
+	}
+
+	private static Cenario montarCenario1() {
 		Cenario cenario = new Cenario();
 		cenario.setLinhas(7);
 		cenario.setColunas(6);
@@ -23,71 +135,137 @@ public class MapaTrapezoidal {
 		cenario.addPontoInvalido(new Ponto(5, 2));
 		cenario.addPontoInvalido(new Ponto(6, 3));
 		cenario.addPontoInvalido(new Ponto(6, 5));
-		mapa.valorarGrafo(cenario);
-		/*
-		 * for (int i = 0; i < mapa.pontos.length; i++) { String str = ""; for
-		 * (int j = 0; j < mapa.pontos[i].length; j++) { str +=
-		 * mapa.pontos[i][j] + " "; } System.out.println(str); }
-		 */
+		return cenario;
 	}
 
-	private void valorarGrafo(final Cenario cenario) {
+	private static void inverter() {
+		Motor.A.rotate(-MAX, true);
+		Motor.C.rotate(MAX);
+	}
+
+	private static void giraParaEsquerda() {
+		Motor.A.rotate(NORMAL, true);
+		Motor.C.rotate(-NORMAL);
+	}
+
+	private static void giraParaDirieta() {
+		Motor.A.rotate(-NORMAL, true);
+		Motor.C.rotate(NORMAL);
+	}
+
+	private void encontrarPassos(final List<Passo> passos, final Ponto ponto, final Ponto ultimoPonto) {
+		Passo p = azulNaMesmaLinhaOuColunaSemObstaculos(ponto);
+		while (p == null) {
+			Passo passo = getVerdeEmAlgumaDirecao(ponto);
+			if (passo != null) {
+				passos.add(passo);
+			} else {
+				passo = getAmareloEmAlgumaDirecao(ponto);
+				if (passo != null) {
+					passos.add(passo);
+				} else {
+					passo = getBrancoEmAlgumaDirecao(ponto);
+					if (passo != null) {
+						passos.add(passo);
+					} else {
+						marcaJafoi(ponto);
+						ponto = ultimoPonto;
+					}
+				}
+			}
+		}
+	}
+
+	private void marcaJafoi(final Ponto ponto) {
+		pontos[ponto.getX()][ponto.getY()] = OBSTACULO;
+	}
+
+	private Passo emAlgumaDirecao(final Ponto ponto, final int cor) {
+		if (ponto.getY() != (pontos[0].length - 1)) {
+			if (pontos[ponto.getX()][ponto.getY() + 1] == cor) {
+				return Passo.DIREITA;
+			}
+		}
+		if (ponto.getY() > 0) {
+			if (pontos[ponto.getX()][ponto.getY() - 1] == cor) {
+				return Passo.ESQUERDA;
+			}
+		}
+		if (ponto.getX() != (pontos.length - 1)) {
+			if (pontos[ponto.getX() + 1][ponto.getY()] == cor) {
+				return Passo.BAIXO;
+			}
+		}
+		if (ponto.getX() > 0) {
+			if (pontos[ponto.getX() - 1][ponto.getY()] == cor) {
+				return Passo.CIMA;
+			}
+		}
+		return null;
+	}
+
+	private Passo getBrancoEmAlgumaDirecao(final Ponto ponto) {
+		return emAlgumaDirecao(ponto, BRANCO);
+	}
+
+	private Passo getAmareloEmAlgumaDirecao(final Ponto ponto) {
+		return emAlgumaDirecao(ponto, AMARELO);
+	}
+
+	private Passo getVerdeEmAlgumaDirecao(final Ponto ponto) {
+		return emAlgumaDirecao(ponto, VERDE);
+	}
+
+	private Passo azulNaMesmaLinhaOuColunaSemObstaculos(final Ponto ponto) {
+		return emAlgumaDirecao(ponto, AZUL);
+	}
+
+	private void calcularPontosMediosEIntersecoes(final Cenario cenario) {
 		pontos = new int[cenario.getLinhas()][cenario.getColunas()];
 		for (Ponto p : cenario.getPontosInvalidos()) {
 			pontos[p.getX()][p.getY()] = -1;
 		}
-		Ponto p = cenario.getInicio();
+		fim = cenario.getFim();
+		inicio = cenario.getInicio();
 
-		List<Ponto> ps = new ArrayList<Ponto>();
-		preencheVizinhos(ps, p);
-		while (ps.size() > 0) {
-			preencheVizinhos(ps, ps.get(0));
-			ps.remove(0);
-		}
-	}
+		pontos[fim.getX()][fim.getY()] = INICIO;
 
-	private void preencheVizinhos(final List<Ponto> ps, final Ponto p) {
-		int value = pontos[p.getX()][p.getY()] + 1;
-		if (p.getX() > 0) {
-			Ponto x = new Ponto(p.getX() - 1, p.getY());
-			if (pontos[x.getX()][x.getY()] == 0) {
-				ps.add(x);
-				pontos[x.getX()][x.getY()] = value;
+		for (int i = 0; i < pontos.length; i++) {
+			int count = 0;
+			for (int j = 0; j < pontos[i].length; j++) {
+				if (pontos[i][j] == OBSTACULO) {
+					if (count > 0) {
+						if ((count % 2) == 0) {
+							pontos[i][(count / 2)] = VERDE;
+						} else {
+							pontos[i][(int) ((count / 2) + 0.5)] = VERDE;
+						}
+					}
+					count = 0;
+				} else {
+					count++;
+				}
 			}
 		}
-		if (p.getY() > 0) {
-			Ponto x = new Ponto(p.getX(), p.getY() - 1);
-			if (pontos[x.getX()][x.getY()] == 0) {
-				ps.add(x);
-				pontos[x.getX()][x.getY()] = value;
-			}
-		}
-		if (p.getX() < (pontos.length - 1)) {
-			Ponto x = new Ponto(p.getX() + 1, p.getY());
-			if (pontos[x.getX()][x.getY()] == 0) {
-				ps.add(x);
-				pontos[x.getX()][x.getY()] = value;
-			}
-		}
-		if (p.getY() < (pontos[p.getX()].length - 1)) {
-			Ponto x = new Ponto(p.getX(), p.getY() + 1);
-			if (pontos[x.getX()][x.getY()] == 0) {
-				ps.add(x);
-				pontos[x.getX()][x.getY()] = value;
+
+		for (int i = 0; i < pontos.length; i++) {
+			for (int j = 0; j < pontos[i].length; j++) {
+				if (((j < (pontos[i].length - 1)) && (pontos[i][j + 1] == VERDE)) || ((j > 0) && (pontos[i][j - 1] == VERDE))) {
+					pontos[i][j] = AMARELO;
+				}
 			}
 		}
 	}
-
 	/*
 	 * 0 - 0 0 0 0 0 0 0 - 0 0 0 0 0 0 - 1 0 0 - 0 0 0 0 0 0 0 - 0 - 0 - 0 0 0 2
 	 * 0 0 - 0 -
-	 *
-	 *
+	 * 
+	 * 
 	 * 0 0 0 0 0 0 0 0 - 0 0 0 0 0 0 - 0 0 1 - 0 0 - 0 0 0 - 0 0 0 0 0 0 0 - 0 0
 	 * 0 0 - 0 1
-	 *
-	 *
-	 *
+	 * 
+	 * 
+	 * 
 	 * 10 -1 10 11 12 13 9 8 9 -1 13 12 8 7 8 9 -1 11 7 6 -1 8 9 10 6 5 6 7 -1
 	 * 11 -1 4 -1 8 9 10 2 3 4 -1 10 -1
 	 */
--- src/dev/Ponto.java
+++ src/dev/Ponto.java
@@ -2,24 +2,33 @@ package dev;
 
 public class Ponto {
 
-	private int x;
-	private int y;
-	
-	public Ponto(int x, int y) {
+	private int	x;
+	private int	y;
+
+	public Ponto(final int x, final int y) {
 		this.x = x;
 		this.y = y;
 	}
+
 	public int getX() {
 		return x;
 	}
-	public void setX(int x) {
+
+	public void setX(final int x) {
 		this.x = x;
 	}
+
 	public int getY() {
 		return y;
 	}
-	public void setY(int y) {
+
+	public void setY(final int y) {
 		this.y = y;
 	}
-	
+
+	@Override
+	public boolean equals(final int x; final int y) {
+		return (getX() == x) && (getY() == y);
+	}
+
 }
